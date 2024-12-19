95db773e9cd171f0d6f2b4371034b8bacc70ef77
weebz <lovesoju05@gmail.com>
2024-12-19 18:00:01 +0000

    Update data.js

--- AutoRatter/data.js
+++ AutoRatter/data.js
@@ -1,6 +1,6 @@
 import PogObject from "PogData";
 
-export const modulePrefix = "&cWith love, from dizzyyss"
+export const modulePrefix = "&cAutoRatter"
 export const modulePrefixU = ChatLib.removeFormatting(modulePrefix)
 export const moduleChat = (text) => { ChatLib.chat(`&6[${modulePrefix}&6] ${text}`) }
 
